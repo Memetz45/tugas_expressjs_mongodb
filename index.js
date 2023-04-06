070e858273776b01a8d99d0e5e29d8c9005537f1
Memetz45 <slametnurhidayat45@gmail.com>
2023-04-06 23:52:45 +0700

    remove /api

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(logger('dev'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use('/public', express.static(path.join(__dirname, 'uploads')));
-app.use('/api/v1', productRouterV1)
-app.use('/api/v2', productRouterV2)
+app.use('/v1', productRouterV1)
+app.use('/v2', productRouterV2)
 app.use((req, res) => {
     res.status(404);
     res.send({
